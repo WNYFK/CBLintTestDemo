9741f711d8e59fbdb4a9ecca25556bf27461f195
ChenBin <chenbin03@kuaishou.com>
2021-11-02 14:47:12 +0800

    demo danger

--- danger/Dangerfile.swift
+++ danger/Dangerfile.swift
@@ -6,3 +6,5 @@ message("These files have changed: \(editedFiles.first ?? "")")
 let tmpPath = editedFiles.first(where: { $0.contains(".swift") }) ?? ""
 message("aaa:\(tmpPath)")
 fail(message: "测试", file: "\(tmpPath)", line: 18)
+warn(message: "警告测试", file: "\(tmpPath)", line: 19)
+
